d33b26d2de5078cb391ce7aabe65184d186a1553
zipnix <78799489+zipnix@users.noreply.github.com>
2021-03-09 11:09:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,12 @@
 int gcd(int a, int b) {
 // поместить сюда текст реализации алгоритма (на С++)
      while (a != b) {
-    if (a > b) {
-      a = a-b;
-    }
-    else {
-      b = b-a;
-    }
-  }
-  return a;
+          if (a > b) {
+               a = a-b;
+          }
+          else {
+               b = b-a;
+          }
+     }
+     return a;
 }
